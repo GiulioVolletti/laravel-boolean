6b59c2d6ffe7c95707f4b26b0f0fce0b681abd74
Giulio <giuliovolletti@hotmail.com>
2021-02-09 16:06:51 +0100

    commit 1

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cars;
+
+class CarsController extends Controller
+{
+  public function index() {
+
+    // select all Cars
+    $cars = Cars::all();
+    dd($cars);
+
+
+    return view('home');
+  }
+}
