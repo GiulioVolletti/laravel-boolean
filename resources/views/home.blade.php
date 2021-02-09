6a5f0ceb0bb00eabafaf12c5238dc8068131ce6e
Giulio <giuliovolletti@hotmail.com>
2021-02-09 17:03:55 +0100

    commit 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,24 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>laravel boolean</title>
+    
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
   </head>
   <body>
 
     <h1> Macchine </h1>
 
-    @foreach ($cars as $car)
-    <div>      
-      <h2>{{$car['name']}}</h2>
-      <h2>{{$car['tipe']}}</h2>
-      <h2>{{$car['model']}}</h2>
+    <div class="container">
+      @foreach ($cars as $car)
+        <div class="car-stile">      
+          <h2>{{$car['name']}}</h2>
+          <h2>{{$car['tipe']}}</h2>
+          <h2>{{$car['model']}}</h2>
+        </div>      
+      @endforeach
     </div>
-    <hr>
-    @endforeach
+    
 
   </body>
 </html>
